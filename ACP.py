be0a47676091a188821b4320e0f89fac8f4a382d
TahmidArs <tahmid.zaman.glab@gmail.com>
2025-10-02 22:17:14 +0600

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,11 @@
+#Write a program to store birthday dates inside 5 different variables.
+x="30th April 2010"
+print(x)
+y="10th of December 2012"
+print(y)
+z="18 October 2010"
+print(z)
+p="December 30,2006"
+print(p)
+q="January 30,2016"
+print(q)
