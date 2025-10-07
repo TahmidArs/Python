dcb66335c1ef8745f08524fb68412cb6ec9b9a91
TahmidArs <tahmid.zaman.glab@gmail.com>
2025-10-07 22:13:10 +0600

    Add files via upload

---
+++ Finding the average.py
@@ -0,0 +1,8 @@
+tree1=98
+tree2=56
+tree3=75
+tree4=85
+sum=tree1+tree2+tree3+tree4
+print("The sum of the height of the trees is;",sum)
+average=sum/4
+print("The average height of the trees is:",average)
---
+++ percentage.py
@@ -0,0 +1,10 @@
+print("Enter marks obtained in 5 subjects:")
+Math=int(input("Math:"))
+Higher_Math=int(input("Higher Math:"))
+Physics=int(input("Physics:"))
+Chemistry=int(input("Chemistry:"))
+Biology=int(input("Biology:"))
+sum=Math+Higher_Math+Physics+Chemistry+Biology
+print("sum of math,higher math,physics,chemistry and biology:",sum)
+percentage=(sum/500)*100
+print("The percentage of the Mark:",percentage)
