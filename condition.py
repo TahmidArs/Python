6de611ca3cbc7403d37552ded7eeb1a6789fb667
TahmidArs <tahmid.zaman.glab@gmail.com>
2025-10-09 22:07:58 +0600

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,8 @@
+i=int(input("Enter a number:"))
+if i>18:
+    print("i is greater than 18")
+    print("i'm in if block.")
+else:
+    print("i is smaller than 18")
+    print("i'm in else block.")
+print("i'm not in if and not in else block.")
---
+++ odd or even.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number to check:"))
+print("The number to be checked is:",num)
+if num%2==0:
+    print(num,"is an even number.")
+else:
+    print(num,"is a odd number.")
