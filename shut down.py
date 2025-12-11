71a0ce40ffd91a3dffe4fdfe10f73416529cefc3
TahmidArs <tahmid.zaman.glab@gmail.com>
2025-12-11 14:31:23 +0600

    Add files via upload

---
+++ shut down.py
@@ -0,0 +1,5 @@
+all_programmes_closed=str(input("Have you closed all proramme y or n:"))
+if all_programmes_closed=="y":
+    print("shutting down the system")
+elif all_programmes_closed=="n":
+    print("sorry,can't shut down")
