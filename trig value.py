2faeb17331bec4855348edc8c8a6bd7cfc13836a
TahmidArs <tahmid.zaman.glab@gmail.com>
2025-12-15 01:37:31 +0600

    Add files via upload

---
+++ trig value.py
@@ -0,0 +1,8 @@
+import math
+angle_in_radians=float(input("Enter angle in radians:"))
+result_sin=math.sin(angle_in_radians)
+result_cos=math.cos(angle_in_radians)
+result_tan=math.tan(angle_in_radians)
+print("The sine value is:",result_sin)
+print("The cosine value is:",result_cos)
+print("The tangent value is:",result_tan)
