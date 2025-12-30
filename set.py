bda10831d705adcfcf647a1cfe27e6c1ab13fe58
TahmidArs <tahmid.zaman.glab@gmail.com>
2025-12-30 21:51:19 +0600

    Add files via upload

---
+++ set.py
@@ -0,0 +1,12 @@
+my_set={1,2,2,3,4}
+print(my_set)
+my_set={1.0,2,'hello',(1,2,3)}
+print(my_set)
+my_set={1,2,3,4,3,2}
+print(my_set)
+my_set=set([1,2,3,4,4,3])
+print(my_set,"\n")
+num_set=set([0,2,3,1,5])
+print("Original set:",num_set)
+num_set.pop()
+print("After remonving first element from the said set:",num_set)
