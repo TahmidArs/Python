8b4e0ec50dc5cbba6e7fa1f3a0637844e321e469
TahmidArs <tahmid.zaman.glab@gmail.com>
2025-12-30 22:01:47 +0600

    Add files via upload

---
+++ set intersection.py
@@ -0,0 +1,12 @@
+setx={'green','blue'}
+sety={'blue','red'}
+print(setx)
+print(sety)
+setz=setx.intersection(sety)
+print("Intersection of setx and sety is:",setz)
+set1={1,2,2,3,4}
+set2={2,3,4,5}
+print(set1)
+print(set2)
+set3=set1.union(set2)
+print("Union of the set1 and set2 is:",set3)
