6de2b9f1d5e0587d82480a54df64c673a070bda0
TahmidArs <tahmid.zaman.glab@gmail.com>
2026-02-17 22:29:13 +0600

    Add files via upload

--- space invader.py
+++ space invader.py
@@ -64,3 +64,73 @@ def fire_bullet(x,y):
     screen.blit(bulletimg,(x+16,y+10))
 def isCollision(enemyx,enemyy,bulletx,bullety):
     distance=math.sqrt(math.pow(enemyx-bulletx,2)+math.pow(enemyy-bullety,2))
+    if distance < 27:
+        return True
+    else:
+        return False
+running=True
+while running:
+    screen.fill((0,0,0))
+    #background image
+    screen.blit(background,(0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_LEFT:
+                playerX_change=-5
+            if event.key==pygame.K_RIGHT:
+                playerX_change=5
+            if event.key==pygame.K_SPACE:
+                if bullet_state is "ready":
+                    bulletSound=mixer.Sound("Laser.wav")
+                    bulletSound.play()
+                    #get the current x coordinate of the spaceship
+                    bulletx=playerx
+                    fire_bullet(bulletx,bullety)
+        if event.type==pygame.KEYUP:
+            if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
+                playerX_change=0
+
+    playerx+=playerX_change
+    if playerX<=0:
+        playerX=0
+    elif playerX>=736:
+        playerX=736
+
+        #enemy movement
+    for i in range(num_of_enemies):
+
+        #Game Over
+        if enemyy[i]>340:
+            for j in range(num_of_enemies):
+                enemyY[j]=2000
+            game_over_text()
+            break
+        enemyy[i]+=enemyy_change[i]
+        if enemyx[i]<=0:
+            enemyx_change[i]=4
+            enemyy[i]+=enemyy_change[i]
+        elif enemyx[i]>=736:
+            enemyx_change[i]=-4
+            enemyy[i]+=enemyy_change[i]
+        collision=isCollision(enemyx[i],enemyy[i],bulletx,bullety)
+        if collision:
+            explosionSound=mixer.Sound("explosion.wav")
+            explosionSound.play()
+            bullety=480
+            bullet_state="ready"
+            score_value+=1
+        enemy(enemyx[i],enemyy[i],i)
+
+    if bullety<=0:
+        bullety=380
+        bullet_state=ready
+    if bullet_state is "fire":
+        fire_bullet(bulletx,bullety)
+        bullety-=bullety_change
+        
+    player(playerx,playery)
+    show_score(textX,textY)
+    pygame.display.update
